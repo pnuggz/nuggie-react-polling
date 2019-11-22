3db6b2f1513673064933131c892f2963b9a0ab4d
Ryan <pnugraha89@gmail.com>
2019-11-23 09:06:55 +1100

    Revert "add props"
    This reverts commit 6a9cfb2bade0cdbfb0f2e1408c796bd4979fd317.

--- src/index.js
+++ src/index.js
@@ -1,64 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import ModalController from './modalController';
-
-export const propTypes = {
-  isLoading: PropTypes.bool.isRequired,
-  isShowing: PropTypes.bool.isRequired,
-  onModalCloseClick: PropTypes.func.isRequired,
-  header: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  footer: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  loader: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  closeIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  modalClass: PropTypes.string,
-  modalClassName: PropTypes.string,
-};
-
-export const defaultProps = {
-  header: null,
-  children: null,
-  footer: null,
-  loader: null,
-  closeIcon: null,
-  modalClass: '',
-  modalClassName: '',
-};
-
-const Modal = props => {
-  const {
-    isLoading,
-    isShowing,
-    onModalCloseClick,
-    header,
-    children,
-    footer,
-    loader,
-    closeIcon,
-    modalClassName,
-    modalClass,
-  } = props;
-
-  const combinedModalClassName = `${modalClass} ${modalClassName}`.replace(/ +(?= )/g, '').trim();
+const Example = props => {
+  const { sample } = props;
 
   return (
-    <ModalController
-      isLoading={isLoading}
-      isShowing={isShowing}
-      onModalCloseClick={onModalCloseClick}
-      header={header}
-      footer={footer}
-      loader={loader}
-      modalClass={combinedModalClassName}
-      closeIcon={closeIcon}
-    >
-      {children}
-    </ModalController>
+    <React.Fragment>
+      <div sample={sample}>Test</div>
+    </React.Fragment>
   );
 };
 
-Modal.propTypes = propTypes;
-Modal.defaultProps = defaultProps;
+Example.propTypes = {
+  sample: PropTypes.string,
+};
 
-export default Modal;
+Example.defaultProps = {
+  sample: null,
+};
+
+export default Example;
